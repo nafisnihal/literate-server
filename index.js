21b42a307e95c75d8aadec7ebd37bc5ea41d945f
nafisnihal <nafisnihal12@gmail.com>
2022-10-25 23:53:29 +0600

    literate server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+app.get("/", (req, res) => {
+  res.send("Literate server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
