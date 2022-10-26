8b9db9d9837d4204993c6a6456890cd1077483c1
nafisnihal <nafisnihal12@gmail.com>
2022-10-26 15:13:10 +0600

    literate server

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 
+const categories = require('./data/categories.json');
 
 app.get("/", (req, res) => {
   res.send("Literate server is running");
 });
+
+app.get('/course-categories', (req, res) =>{
+    res.send(categories)
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
